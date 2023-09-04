14a5a59e10f5f13ec36c9e56c87bc3d86af8fa0e
Leonel Oliveira <leonel.oliveira@volarisgroup.com>
2023-09-04 17:47:02 -0400

    Small fixes for the header menu

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -80,6 +80,13 @@ function SearchForm({ searchQuery, setSearchQuery, handleSubmit }: SearchFormPro
                 fill="none"
                 viewBox="0 0 17 14"
               >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
               </svg>
             </button>
           </div>
